59acdf2f7e4312a829939fc9b87654bc4427bcc1
ketto-medical-donations <yashas7722@gmail.com>
2026-03-26 11:20:58 +0530

    Update webcamapp.py

--- webcamapp.py
+++ webcamapp.py
@@ -5,24 +5,23 @@ from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 
 st.title("🧠 Pose Detection (Browser Camera)")
 
-mp_pose = mp.solutions.pose
-mp_draw = mp.solutions.drawing_utils
+# NEW mediapipe import
+from mediapipe.tasks.python import vision
+from mediapipe.tasks.python.vision import PoseLandmarker, PoseLandmarkerOptions
+from mediapipe.tasks import python
+
+BaseOptions = python.BaseOptions
 
 class PoseDetector(VideoTransformerBase):
     def __init__(self):
-        self.pose = mp_pose.Pose()
+        options = PoseLandmarkerOptions(
+            base_options=BaseOptions(model_asset_path="pose_landmarker.task"),
+            running_mode=vision.RunningMode.VIDEO
+        )
+        self.detector = PoseLandmarker.create_from_options(options)
 
     def transform(self, frame):
         img = frame.to_ndarray(format="bgr24")
+        return img  # (basic version for now)
 
-        rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        result = self.pose.process(rgb)
-
-        if result.pose_landmarks:
-            mp_draw.draw_landmarks(
-                img, result.pose_landmarks, mp_pose.POSE_CONNECTIONS
-            )
-
-        return img
-
-webrtc_streamer(key="pose-detection", video_transformer_factory=PoseDetector)
+webrtc_streamer(key="pose", video_transformer_factory=PoseDetector)
